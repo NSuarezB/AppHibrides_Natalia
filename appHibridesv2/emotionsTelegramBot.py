1029a5dfc89b5e9de9b73027637f6e01e242491a
NSuarezB <56040550+NSuarezB@users.noreply.github.com>
2019-11-13 17:17:06 +0100

    telegram bot v2 llegir i escriure files

--- appHibridesv2/emotionsTelegramBot.py
+++ appHibridesv2/emotionsTelegramBot.py
@@ -7,8 +7,7 @@ import json
 bot = telebot.TeleBot("881906354:AAGCRz33rl_75jd0clXDcr3urHWLPMXWPeg")
 
 
-bot.send_message(965523336, "Hola, benvenido/da! <3")
-bot.send_message(965523336, "Como te va hoy? ")
+
 
 """ Aquests send_message s'activen nomes obrir el bot """
 resp1="Eso no es bueno T-T' \nAquí te dejo una frase para que te animes ~^-^~"
@@ -33,14 +32,48 @@ json_keyboard = json.dumps({'keyboard': [["Bien"], ["Mal"]],
 
 
 
-json_keyboard2 = json.dumps({'keyboard': [["Si"], ["No"]],
+json_keyboard2 = json.dumps({'keyboard': [["feliz.txt"], ["trist.txt"]],
                             'one_time_keyboard': True,
                             'resize_keyboard': True})
 
 
+@bot.message_handler(regexp="adios")
+# Emociones felices=FreasesFeliz
+def adios(message):
+    bot.send_message(message.chat.id, "Hasta pronto o/")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Hola, benvenido/da! <3")
+    bot.send_message(message.chat.id, "Como te va hoy? ")
+    bot.send_message(message.chat.id, "Si no sabes que hacer escribe /help")
+
+@bot.message_handler(commands=['help'])
+def help_me(message):
+    bot.send_message(message.chat.id, "Puedes decirme como te fue hoy el dia")
+    bot.send_message(message.chat.id, "Tambien puedes añadir frases con /add")
 
 
+@bot.message_handler(commands=['add'])
+def add_frase(message):
 
+    bot.send_message(message.chat.id,"En que archivo la quieres guardar?",reply_markup=json_keyboard2)
+    archivo = message.text
+    arxiuGeneral = open(str(archivo),'a')
+
+    def listener(message):
+
+       for word in message:
+
+           bot.send_message(world.chat.id, "Que frase quieres añadir?")
+           frase=word.text
+
+
+           arxiuGeneral.write(frase+"\n")
+           arxiuGeneral.close()
+
+
+    bot.set_update_listener(listener)
 
 @bot.message_handler(func=lambda message:True)
 
@@ -81,10 +114,7 @@ def compro(message):
     #        bot.send_message(message.chat.id,"Estas bien o mal?",reply_markup=json_keyboard)
 
 
-@bot.message_handler(regexp="adios")
-# Emociones felices=FreasesFeliz
-def adios(message):
-    bot.send_message(message.chat.id, "Hasta pronto o/")
+
 
 
 " Aquesta funcio s'activa quan troba les paraules 'triste' o 'deprimido'. Utilitza l'array frasesTristes """
@@ -115,4 +145,5 @@ def control(message):
 """
 arxiuTrist.close()
 arxiuFeliz.close()
+
 bot.polling()
